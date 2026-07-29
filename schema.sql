c5f1fa3cf25398663e6c96ff1a973dda9710dfe3
airusdev <work.airus@gmail.com>
2026-07-29 21:01:48 +0800

    initial tables

---
+++ schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE students (
+    student_id int,
+    student_name varchar(80)
+);
+
+CREATE TABLE courses (
+    course_id int,
+    course_name varchar(80),
+    course_capacity int
+);
+
+CREATE TABLE enrollments (
+    student_id int,
+    course_id int,
+    date_enrolled date
+);
+
+CREATE TABLE school_inventory (
+    item varchar(80),
+    classification varchar(80),
+    item_quantity int
+);
